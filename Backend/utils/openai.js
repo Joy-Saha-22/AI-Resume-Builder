34d08acfe28ab0e6178d9b89d6df57c17838c63a
Joy-Saha-22 <joysaha22.dev@gmail.com>
2026-09-11 23:30:54 +0530

    using openai's model

--- Backend/utils/openai.js
+++ Backend/utils/openai.js
@@ -11,7 +11,7 @@ export const openai = new OpenAI({
 export const getOpenAIResponse = async (prompt) => {
   try {
     const response = await openai.chat.completions.create({
-      model:"nvidia/nemotron-3-ultra:free",
+      model:"openai/gpt-oss-20b:free",
       messages: [
         {
           role: "system",
