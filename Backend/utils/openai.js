4cedf46c6574734ed501770869d3188e3677379a
Joy-Saha-22 <joysaha22.dev@gmail.com>
2026-09-11 20:55:21 +0530

    Update openai.js

--- Backend/utils/openai.js
+++ Backend/utils/openai.js
@@ -11,8 +11,8 @@ export const openai = new OpenAI({
 export const getOpenAIResponse = async (prompt) => {
   try {
     const response = await openai.chat.completions.create({
-      model: "nvidia/nemotron-3-ultra:free", 
-      // model:"chatgpt-4o-latest",
+      // model: "nvidia/nemotron-3-ultra:free", 
+      model:"chatgpt-4o-latest",
       messages: [
         {
           role: "system",
